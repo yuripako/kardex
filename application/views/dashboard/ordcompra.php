f62d85f808804249476321033697152a45da3376
EDGAR <yuripako@gmail.com>
2019-08-12 10:56:42 -0500

    cambios compra

--- application/views/dashboard/ordcompra.php
+++ application/views/dashboard/ordcompra.php
@@ -124,7 +124,7 @@
         </div>
         <div class="modal-footer">
           <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
-          <a  class="btn btn-primary" href="#" onclick="agregar_categoria();">Agregar Producto</a>
+         
         </div>
       </div>
     </div>
